a67c5a758d6b9d1075dbf888f7f42cfaec279c8d
Alberto González <albertogonzalezcatalan@gmail.com>
2020-03-09 17:07:40 +0100

    leccion04

--- wp-content/plugins/hello.php
+++ wp-content/plugins/hello.php
@@ -99,3 +99,119 @@ function dolly_css() {
 }
 
 add_action( 'admin_head', 'dolly_css' );
+
+// Modificaciones Hello Dolly
+function hello_dolly_menu()
+{
+	add_menu_page('Hello Dolly','Hello Dolly','manage_options','/widgets.php');
+}
+add_action( 'admin_menu', 'hello_dolly_menu' );
+
+class HelloDollyWidget extends WP_Widget {
+	public function __construct() {
+		// actual widget processes
+		$params = array (
+			'classname' => 'HelloDollyWidget',
+			'description' => 'Mostramos frases del plugin Hello Dolly'
+		   );
+		  
+		parent::__construct( 'HelloDollyWidget', 'HelloDolly', $params);
+		  
+    }
+ 
+    public function widget( $args, $instance ) {
+		// outputs the content of the widget
+		extract( $args );
+        $title = apply_filters( 'widget_title', $instance['title'] );
+ 
+        echo $before_widget;
+        if ( ! empty( $title ) ) {
+            echo $before_title . $title . $after_title;
+        }
+		$this->hello_dolly();
+		echo $after_widget;
+    }
+ 
+    public function form( $instance ) {
+		// outputs the options form in the admin
+		if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'Nuevo título', '' );
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+         </p>
+    <?php
+    }
+ 
+    public function update( $new_instance, $old_instance ) {
+        // processes widget options to be saved
+		$instance = $old_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		return $instance;     
+	}
+	
+	function hello_dolly_get_lyric() {
+		/** These are the lyrics to Hello Dolly */
+		$lyrics = "Hello, Dolly
+	Well, hello, Dolly
+	It's so nice to have you back where you belong
+	You're lookin' swell, Dolly
+	I can tell, Dolly
+	You're still glowin', you're still crowin'
+	You're still goin' strong
+	I feel the room swayin'
+	While the band's playin'
+	One of our old favorite songs from way back when
+	So, take her wrap, fellas
+	Dolly, never go away again
+	Hello, Dolly
+	Well, hello, Dolly
+	It's so nice to have you back where you belong
+	You're lookin' swell, Dolly
+	I can tell, Dolly
+	You're still glowin', you're still crowin'
+	You're still goin' strong
+	I feel the room swayin'
+	While the band's playin'
+	One of our old favorite songs from way back when
+	So, golly, gee, fellas
+	Have a little faith in me, fellas
+	Dolly, never go away
+	Promise, you'll never go away
+	Dolly'll never go away again";
+	
+		// Here we split it into lines.
+		$lyrics = explode( "\n", $lyrics );
+	
+		// And then randomly choose a line.
+		return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+	}
+	
+	// This just echoes the chosen line, we'll position it later.
+	function hello_dolly() {
+		$chosen = hello_dolly_get_lyric();
+		$lang   = '';
+		if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
+			$lang = ' lang="en"';
+		}
+	
+		printf(
+			'<p id="dolly"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
+			__( 'Quote from Hello Dolly song, by Jerry Herman:' ),
+			$lang,
+			$chosen
+		);
+	}
+	
+}
+
+function hello_dolly_register_widget()
+{
+	register_widget( 'HelloDollyWidget' );
+}
+add_action( 'widgets_init', 'hello_dolly_register_widget' );
