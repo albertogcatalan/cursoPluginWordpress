c8ddbc7c279ed6ba41b95233a377e1c93b4b885d
Alberto González <alberto@gcatalan.com>
2020-09-25 09:38:28 +0200

    iiub

--- wp-content/themes/twentyseventeen/header.php
+++ wp-content/themes/twentyseventeen/header.php
@@ -17,14 +17,9 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="profile" href="https://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
-<script>
-  function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-  }
-</script>
 </head>
 
 <body <?php body_class(); ?>>
